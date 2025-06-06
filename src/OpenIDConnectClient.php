4199c449cb7cdc8e945a96767c3c8d2ae7d44d12
Vassilis Panos <4130346+vassilis-panos@users.noreply.github.com>
2025-06-06 09:27:15 +0300

    Fixes namespace

--- src/OpenIDConnectClient.php
+++ src/OpenIDConnectClient.php
@@ -1,6 +1,26 @@
 <?php
+/**
+ *
+ * Copyright MITRE 2020
+ *
+ * OpenIDConnectClient for PHP7+
+ * Author: Michael Jett <mjett@mitre.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may
+ * not use this file except in compliance with the License. You may obtain
+ * a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ */
 
-namespace accounts;
+namespace Jumbojett;
 
 use Error;
 use Exception;
